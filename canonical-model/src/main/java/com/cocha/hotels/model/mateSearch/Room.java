0f2f9850f52ce7860e0218f2a87a072236241526
elfrasco <adrianparedes82@gmail.com>
2015-09-05 17:37:01 -0300

    Primer Modelo para el llamdo de los servicios. Modelo Basado en la respuesta de EAN.

---
+++ canonical-model/src/main/java/com/cocha/hotels/model/mateSearch/Room.java
@@ -0,0 +1,28 @@
+package com.cocha.hotels.model.mateSearch;
+
+public class Room {
+	
+	private Long idRoom;
+	private Long idHotelMapper;
+	private RateInfo rateInfo;
+	
+	public Long getIdRoom() {
+		return idRoom;
+	}
+	public void setIdRoom(Long idRoom) {
+		this.idRoom = idRoom;
+	}
+	public Long getIdHotelMapper() {
+		return idHotelMapper;
+	}
+	public void setIdHotelMapper(Long idHotelMapper) {
+		this.idHotelMapper = idHotelMapper;
+	}
+	public RateInfo getRateInfo() {
+		return rateInfo;
+	}
+	public void setRateInfo(RateInfo rateInfo) {
+		this.rateInfo = rateInfo;
+	}
+	
+}
---
+++ canonical-model/src/main/java/com/cocha/hotels/model/mateSearch/hotel.java
@@ -0,0 +1,46 @@
+package com.cocha.hotels.model.mateSearch;
+
+import java.util.Date;
+import java.util.List;
+
+public class hotel {
+
+	private Long idMapper;
+	private String hotelName;
+	private Date arrivalDate;
+	private Date departureDate;
+	private List<Room> rooms;
+	
+	
+	public Long getIdMapper() {
+		return idMapper;
+	}
+	public void setIdMapper(Long idMapper) {
+		this.idMapper = idMapper;
+	}
+	public String getHotelName() {
+		return hotelName;
+	}
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+	public Date getArrivalDate() {
+		return arrivalDate;
+	}
+	public void setArrivalDate(Date arrivalDate) {
+		this.arrivalDate = arrivalDate;
+	}
+	public Date getDepartureDate() {
+		return departureDate;
+	}
+	public void setDepartureDate(Date departureDate) {
+		this.departureDate = departureDate;
+	}
+	public List<Room> getRooms() {
+		return rooms;
+	}
+	public void setRooms(List<Room> rooms) {
+		this.rooms = rooms;
+	}
+	
+}
