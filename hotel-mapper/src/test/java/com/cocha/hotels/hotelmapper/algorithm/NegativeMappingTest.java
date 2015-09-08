15f9bcc848862e50f170c1a4411b9882dc75a047
germanio <german.hohmann@gmail.com>
2015-09-08 13:13:13 -0300

    Test negativos entre Armada hotel y los otros.

--- hotel-mapper/src/test/java/com/cocha/hotels/hotelmapper/algorithm/NegativeMappingTest.java
+++ hotel-mapper/src/test/java/com/cocha/hotels/hotelmapper/algorithm/NegativeMappingTest.java
@@ -5,7 +5,16 @@ import org.junit.Test;
 
 import com.cocha.hotels.hotelmapper.mocks.ArmadaHotelMock;
 import com.cocha.hotels.hotelmapper.mocks.BlackstoneHotelMock;
+import com.cocha.hotels.hotelmapper.mocks.ComfortInnHotelMock;
 import com.cocha.hotels.hotelmapper.mocks.HotelMock;
+import com.cocha.hotels.hotelmapper.mocks.LeamingtonHotelMock;
+import com.cocha.hotels.hotelmapper.mocks.QualitySuitesHotelMock;
+import com.cocha.hotels.hotelmapper.mocks.QualitySuitesUSHotelMock;
+import com.cocha.hotels.hotelmapper.mocks.SaintEugeneHotelMock;
+import com.cocha.hotels.hotelmapper.mocks.StaybridgeSuitesHotelMock;
+import com.cocha.hotels.hotelmapper.mocks.TaybridgeSuitesHotelMock;
+import com.cocha.hotels.hotelmapper.mocks.TravelodgeFlagstaffHotelMock;
+import com.cocha.hotels.hotelmapper.mocks.WallStreet_HI_HotelMock;
 import com.cocha.hotels.model.content.hotel.Hotel;
 
 /**
@@ -36,6 +45,186 @@ public class NegativeMappingTest extends BaseHotelMappingTest {
     @Test
     public void testArmadaAgainstBlackstoneAddress() {
         HotelMock builder = new BlackstoneHotelMock();
+        Hotel bookingHotel = builder.buildWithBooking();
+
+        HotelRule rule = new HotelAddressRule();
+        RuleMatch ruleMatch = rule.apply(reference, bookingHotel);
+        verify(ruleMatch).isNotSuccessful();
+    }
+
+    @Test
+    public void testArmadaAgainstComfortInnName() {
+        HotelMock builder = new ComfortInnHotelMock();
+        Hotel bookingHotel = builder.buildWithBooking();
+
+        HotelRule rule = new HotelNameRule();
+        RuleMatch ruleMatch = rule.apply(reference, bookingHotel);
+        verify(ruleMatch).isNotSuccessful();
+    }
+
+    @Test
+    public void testArmadaAgainstComfortInnAddress() {
+        HotelMock builder = new ComfortInnHotelMock();
+        Hotel bookingHotel = builder.buildWithBooking();
+
+        HotelRule rule = new HotelAddressRule();
+        RuleMatch ruleMatch = rule.apply(reference, bookingHotel);
+        verify(ruleMatch).isNotSuccessful();
+    }
+
+    @Test
+    public void testArmadaAgainstLeamingtonName() {
+        HotelMock builder = new LeamingtonHotelMock();
+        Hotel bookingHotel = builder.buildWithBooking();
+
+        HotelRule rule = new HotelNameRule();
+        RuleMatch ruleMatch = rule.apply(reference, bookingHotel);
+        verify(ruleMatch).isNotSuccessful();
+    }
+
+    @Test
+    public void testArmadaAgainstLeamingtonAddress() {
+        HotelMock builder = new LeamingtonHotelMock();
+        Hotel bookingHotel = builder.buildWithBooking();
+
+        HotelRule rule = new HotelAddressRule();
+        RuleMatch ruleMatch = rule.apply(reference, bookingHotel);
+        verify(ruleMatch).isNotSuccessful();
+    }
+
+    @Test
+    public void testArmadaAgainstQualityName() {
+        HotelMock builder = new QualitySuitesHotelMock();
+        Hotel bookingHotel = builder.buildWithBooking();
+
+        HotelRule rule = new HotelNameRule();
+        RuleMatch ruleMatch = rule.apply(reference, bookingHotel);
+        verify(ruleMatch).isNotSuccessful();
+    }
+
+    @Test
+    public void testArmadaAgainstQualityAddress() {
+        HotelMock builder = new QualitySuitesHotelMock();
+        Hotel bookingHotel = builder.buildWithBooking();
+
+        HotelRule rule = new HotelAddressRule();
+        RuleMatch ruleMatch = rule.apply(reference, bookingHotel);
+        verify(ruleMatch).isNotSuccessful();
+    }
+
+    @Test
+    public void testArmadaAgainstQualityUSName() {
+        HotelMock builder = new QualitySuitesUSHotelMock();
+        Hotel bookingHotel = builder.buildWithBooking();
+
+        HotelRule rule = new HotelNameRule();
+        RuleMatch ruleMatch = rule.apply(reference, bookingHotel);
+        verify(ruleMatch).isNotSuccessful();
+    }
+
+    @Test
+    public void testArmadaAgainstQualityUSAddress() {
+        HotelMock builder = new QualitySuitesUSHotelMock();
+        Hotel bookingHotel = builder.buildWithBooking();
+
+        HotelRule rule = new HotelAddressRule();
+        RuleMatch ruleMatch = rule.apply(reference, bookingHotel);
+        verify(ruleMatch).isNotSuccessful();
+    }
+
+    @Test
+    public void testArmadaAgainstSaintEugeneName() {
+        HotelMock builder = new SaintEugeneHotelMock();
+        Hotel bookingHotel = builder.buildWithBooking();
+
+        HotelRule rule = new HotelNameRule();
+        RuleMatch ruleMatch = rule.apply(reference, bookingHotel);
+        verify(ruleMatch).isNotSuccessful();
+    }
+
+    @Test
+    public void testArmadaAgainstSaintEugeneAddress() {
+        HotelMock builder = new SaintEugeneHotelMock();
+        Hotel bookingHotel = builder.buildWithBooking();
+
+        HotelRule rule = new HotelAddressRule();
+        RuleMatch ruleMatch = rule.apply(reference, bookingHotel);
+        verify(ruleMatch).isNotSuccessful();
+    }
+
+    @Test
+    public void testArmadaAgainstStaybridgeName() {
+        HotelMock builder = new StaybridgeSuitesHotelMock();
+        Hotel bookingHotel = builder.buildWithBooking();
+
+        HotelRule rule = new HotelNameRule();
+        RuleMatch ruleMatch = rule.apply(reference, bookingHotel);
+        verify(ruleMatch).isNotSuccessful();
+    }
+
+    @Test
+    public void testArmadaAgainstStaybridgeAddress() {
+        HotelMock builder = new StaybridgeSuitesHotelMock();
+        Hotel bookingHotel = builder.buildWithBooking();
+
+        HotelRule rule = new HotelAddressRule();
+        RuleMatch ruleMatch = rule.apply(reference, bookingHotel);
+        verify(ruleMatch).isNotSuccessful();
+    }
+
+    @Test
+    public void testArmadaAgainstTaybridgeName() {
+        HotelMock builder = new TaybridgeSuitesHotelMock();
+        Hotel bookingHotel = builder.buildWithBooking();
+
+        HotelRule rule = new HotelNameRule();
+        RuleMatch ruleMatch = rule.apply(reference, bookingHotel);
+        verify(ruleMatch).isNotSuccessful();
+    }
+
+    @Test
+    public void testArmadaAgainstTaybridgeAddress() {
+        HotelMock builder = new TaybridgeSuitesHotelMock();
+        Hotel bookingHotel = builder.buildWithBooking();
+
+        HotelRule rule = new HotelAddressRule();
+        RuleMatch ruleMatch = rule.apply(reference, bookingHotel);
+        verify(ruleMatch).isNotSuccessful();
+    }
+
+    @Test
+    public void testArmadaAgainstTravelodgeName() {
+        HotelMock builder = new TravelodgeFlagstaffHotelMock();
+        Hotel bookingHotel = builder.buildWithBooking();
+
+        HotelRule rule = new HotelNameRule();
+        RuleMatch ruleMatch = rule.apply(reference, bookingHotel);
+        verify(ruleMatch).isNotSuccessful();
+    }
+
+    @Test
+    public void testArmadaAgainstTravelodgeAddress() {
+        HotelMock builder = new TravelodgeFlagstaffHotelMock();
+        Hotel bookingHotel = builder.buildWithBooking();
+
+        HotelRule rule = new HotelAddressRule();
+        RuleMatch ruleMatch = rule.apply(reference, bookingHotel);
+        verify(ruleMatch).isNotSuccessful();
+    }
+
+    @Test
+    public void testArmadaAgainstWallStreetName() {
+        HotelMock builder = new WallStreet_HI_HotelMock();
+        Hotel bookingHotel = builder.buildWithBooking();
+
+        HotelRule rule = new HotelNameRule();
+        RuleMatch ruleMatch = rule.apply(reference, bookingHotel);
+        verify(ruleMatch).isNotSuccessful();
+    }
+
+    @Test
+    public void testArmadaAgainstWallStreetAddress() {
+        HotelMock builder = new WallStreet_HI_HotelMock();
         Hotel bookingHotel = builder.buildWithBooking();
 
         HotelRule rule = new HotelAddressRule();
