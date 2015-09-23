2888c49f13b22c52cdd8986091d71da4fe0175ae
germanio <german.hohmann@gmail.com>
2015-09-22 21:08:15 -0300

    Actualiza distancia del filtro y agrega logger.
    Según los logs, las coordenadas pueden tener diferencias de más de 500m.
    Se setea en 1000m para evitar descartar buenos matches, aunque esto impacta en la
    cantidad de hoteles a procesar, especialmente en áreas turísticas.

--- hotel-mapper/src/main/java/com/cocha/hotels/hotelmapper/mapping/ProximityFilterService.java
+++ hotel-mapper/src/main/java/com/cocha/hotels/hotelmapper/mapping/ProximityFilterService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import com.cocha.hotels.model.content.hotel.Hotel;
@@ -11,14 +12,15 @@ import com.cocha.hotels.model.content.hotel.Hotel;
 @Service
 public class ProximityFilterService {
 
+    private Logger logger = Logger.getLogger(ProximityFilterService.class);
+
     private static final Double EARTH_RADIUS = 6371000d; // meters
     
     //TODO: parametrizar. Distancia en metros
-    private Float maxProximity = 100f;
+    private Float maxProximity = 1000f;
     
     public List<Hotel> filter(Hotel hotel, List<Hotel> hotelsToFilter) {
         Stream<Hotel> filtered = hotelsToFilter.stream().filter((hotelToCompare) -> {
-            
             return maxProximity >= distBetween(hotel, hotelToCompare);
         });
         
@@ -40,8 +42,10 @@ public class ProximityFilterService {
         Double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(Math.toRadians(latReference))
                 * Math.cos(Math.toRadians(latDestination)) * Math.sin(dLng / 2) * Math.sin(dLng / 2);
         Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        Float dist = (float) (EARTH_RADIUS * c);
 
-        return (float) (EARTH_RADIUS * c);
+        logger.info(String.format("[%f %f] [%f %f] -> %f", latReference, lngReference, latDestination, lngDestination, dist));
+        return dist;
     }
 
 }
