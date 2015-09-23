0421cd4995e3e85f40aa6e99dc8d615d8a1a1746
viken <viken.vdm@gmail.com>
2015-09-23 12:45:37 -0300

    Cracion de la pagina de test

---
+++ mate-search-test-ui/mate-search-ui/app/scripts/controllers/service.js
@@ -0,0 +1,40 @@
+var serviceMateSearch = angular.module('mateSearchTestApp');
+
+serviceMateSearch.controller('serviceController', ['$scope','$http','$location', function($scope,$http,$location) {
+	
+	$scope.hotel={};
+	
+	$scope.search = function(hotel) {
+		
+		if(hotel.service == 'all') {
+			$http.get('http://'+$location.host()+':'+$location.port()+'/mate-search/mate/availability?idHotel='+hotel.idSupplier+'&arrival_date='+hotel.arrivalDate+'&departure_date='+hotel.departureDate)
+			.success(function(data) {
+				$scope.hotel = data;
+				$scope.hotel.arrivalDate = hotel.arrivalDate;
+				$scope.hotel.departureDate = hotel.departureDate;
+				$scope.hotel.service = hotel.service;
+			});
+		}
+		if(hotel.service == 'booking') {
+			$http.get('http://'+$location.host()+':'+$location.port()+'/mate-search/booking/send?idHotel='+hotel.idSupplier+'&arrival_date='+hotel.arrivalDate+'&departure_date='+hotel.departureDate)
+			.success(function(data) {
+				$scope.hotel = data;
+				$scope.hotel.arrivalDate = hotel.arrivalDate;
+				$scope.hotel.departureDate = hotel.departureDate;
+				$scope.hotel.service = hotel.service;
+			});		
+		}
+		if(hotel.service == 'ean') {
+			$http.get('http://'+$location.host()+':'+$location.port()+'/mate-search/ean/send?idHotel='+hotel.idSupplier+'&arrival_date='+hotel.arrivalDate+'&departure_date='+hotel.departureDate)
+			.success(function(data) {
+				$scope.hotel = data;
+				$scope.hotel.arrivalDate = hotel.arrivalDate;
+				$scope.hotel.departureDate = hotel.departureDate;
+				$scope.hotel.service = hotel.service;
+			});
+		}
+		if(hotel.service == 'sabre') {
+			
+		}
+	};
+}]);
