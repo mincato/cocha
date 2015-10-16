152faed9650fdc0ea02cc43a8f85f3dbae3fe2ed
viken <viken.vdm@gmail.com>
2015-10-16 15:44:27 -0300

    Clase que no iba

--- mate-search/src/main/java/com/cocha/hotels/matesearch/providers/routes/BookingTransformerRoute.java
+++ mate-search/src/main/java/com/cocha/hotels/matesearch/providers/routes/BookingTransformerRoute.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 
 import com.cocha.hotels.matesearch.providers.processors.ErrorSupplierProcessor;
 import com.cocha.hotels.matesearch.providers.processors.SupplierHotelProcessor;
-import com.cocha.hotels.matesearch.providers.processors.View;
 import com.cocha.hotels.matesearch.util.Constant;
 import com.cocha.hotels.matesearch.util.Constant.CodeSupplier;
 import com.cocha.hotels.model.matesearch.supplier.booking.GetHotelAvailabilityBooking;
@@ -25,7 +24,7 @@ public class BookingTransformerRoute extends RouteBuilder {
     	
     	onException(RuntimeException.class).handled(true).setHeader(Constant.SUPPLIER, simple(CodeSupplier.BOOKING_SUPPLIER_CODE)).process(errorSupplierProcessor).end();
 
-        from("direct:transformerResposeBooking").process(new View()).convertBodyTo(GetHotelAvailabilityBooking.class)
+        from("direct:transformerResposeBooking").convertBodyTo(GetHotelAvailabilityBooking.class)
                 .setHeader("supplier", simple(CodeSupplier.BOOKING_SUPPLIER_CODE)).bean(supplirHotelProcessor);
 
     }
