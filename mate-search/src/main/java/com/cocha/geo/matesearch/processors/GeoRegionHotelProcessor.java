29238ee2ed6942bec65de50d5b444845fe252123
osvaldo_lamas <osvaldolamas@gmail.com>
2015-10-20 10:37:38 -0300

    Se agrega servicio REST para obtener hotelsIds por regionId. Se modifica tabla para feed.

---
+++ mate-search/src/main/java/com/cocha/geo/matesearch/processors/GeoRegionHotelProcessor.java
@@ -0,0 +1,46 @@
+package com.cocha.geo.matesearch.processors;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.cocha.hotels.matesearch.repositories.RegionHotelMappingRepository;
+import com.cocha.hotels.matesearch.util.MessageUtils;
+import com.cocha.hotels.model.content.mapping.RegionHotelMapping;
+
+@Component
+public class GeoRegionHotelProcessor implements Processor {
+
+    @Autowired
+    private RegionHotelMappingRepository regionHotelMappingRepository;
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.apache.camel.Processor#process(org.apache.camel.Exchange)
+     */
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        Message inMessage = exchange.getIn();
+
+        Map<String, Object> headers = inMessage.getHeaders();
+        Map<String, String> parameters = MessageUtils.parseQueryParams((String) headers.get("CamelHttpQuery"));
+        
+        String id = parameters.get("idRegion");        
+        List<RegionHotelMapping> hotelsByRegion = regionHotelMappingRepository.findByRegionId(id);
+        //parameters = this.putIdSuppliers(parameters, providers);
+        String response = "<hotels>";
+        
+        for (RegionHotelMapping regionHotelMapping : hotelsByRegion) {			
+        	response += "<hotel>"+regionHotelMapping.getHotelId()+"</hotel>";
+		}		
+        response += "</hotels>";        
+        exchange.getOut().setBody(response);
+
+    }
+}
---
+++ mate-search/src/main/java/com/cocha/geo/matesearch/routes/GeoRoute.java
@@ -0,0 +1,26 @@
+package com.cocha.geo.matesearch.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import com.cocha.geo.matesearch.processors.GeoRegionHotelProcessor;
+
+/**
+ * Defines the camel routes
+ */
+@Component
+public class GeoRoute extends RouteBuilder {
+    
+    @Autowired
+    private GeoRegionHotelProcessor geoRegionHotelProcessor;
+    
+    @Override
+    public void configure() throws Exception {
+
+        from("cxfrs:bean:geoServer").wireTap("direct:logInfo").choice()
+                .when(simple("${headers.operationName} == 'getHotelsByRegionId'"))
+                .to("direct:getRegionHotel");
+        
+		from("direct:getRegionHotel").process(geoRegionHotelProcessor).wireTap("direct:logInfo");
+    }
+}
