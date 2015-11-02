39a29a051b7ebd3ac06d5326fea0a31f164fea43
elfrasco <adrianparedes82@gmail.com>
2015-11-02 14:12:32 -0300

    Agregados scripts para generar los schemas y el diccionario

---
+++ scripts/content-schema.sql
@@ -0,0 +1,204 @@
+-- MySQL dump 10.13  Distrib 5.6.27, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: content
+-- ------------------------------------------------------
+-- Server version	5.6.27-0ubuntu1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Airport`
+--
+
+DROP TABLE IF EXISTS `Airport`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Airport` (
+  `id` varchar(255) NOT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `countryCode` varchar(255) DEFAULT NULL,
+  `idParentCity` varchar(255) DEFAULT NULL,
+  `latitude` double NOT NULL,
+  `longitude` double NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `DictionaryReplacementRule`
+--
+
+DROP TABLE IF EXISTS `DictionaryReplacementRule`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `DictionaryReplacementRule` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `countryCode` varchar(255) DEFAULT NULL,
+  `hotelAttribute` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Hotel`
+--
+
+DROP TABLE IF EXISTS `Hotel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Hotel` (
+  `id` varchar(255) NOT NULL,
+  `supplierCode` varchar(3) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `countryCode` varchar(255) DEFAULT NULL,
+  `currencyCode` varchar(255) DEFAULT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `starRating` varchar(255) DEFAULT NULL,
+  `zipCode` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`,`supplierCode`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `HotelDescription`
+--
+
+DROP TABLE IF EXISTS `HotelDescription`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `HotelDescription` (
+  `id` varchar(255) NOT NULL,
+  `supplierCode` varchar(3) NOT NULL,
+  `description` longtext,
+  `languageCode` varchar(2) DEFAULT NULL,
+  PRIMARY KEY (`id`,`supplierCode`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `HotelMapping`
+--
+
+DROP TABLE IF EXISTS `HotelMapping`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `HotelMapping` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `active` tinyint(1) NOT NULL,
+  `confidence` int(11) DEFAULT NULL,
+  `hotelId` varchar(255) DEFAULT NULL,
+  `mappedByUser` tinyint(1) NOT NULL,
+  `supplierCode` varchar(3) DEFAULT NULL,
+  `supplierHotelId` varchar(255) DEFAULT NULL,
+  `unmapped` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `NeighborhoodArea`
+--
+
+DROP TABLE IF EXISTS `NeighborhoodArea`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `NeighborhoodArea` (
+  `id` varchar(255) NOT NULL,
+  `coordinates` longtext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Region`
+--
+
+DROP TABLE IF EXISTS `Region`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Region` (
+  `id` varchar(255) NOT NULL,
+  `idParent` varchar(255) DEFAULT NULL,
+  `regionName` varchar(255) DEFAULT NULL,
+  `regionNameLong` varchar(255) DEFAULT NULL,
+  `regionType` varchar(255) DEFAULT NULL,
+  `subClass` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `RegionHotel`
+--
+
+DROP TABLE IF EXISTS `RegionHotel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `RegionHotel` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `idHotel` varchar(255) DEFAULT NULL,
+  `idRegion` varchar(255) DEFAULT NULL,
+  `supplierCode` varchar(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `RegionHotelMapping`
+--
+
+DROP TABLE IF EXISTS `RegionHotelMapping`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `RegionHotelMapping` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `active` tinyint(1) NOT NULL,
+  `confidence` int(11) DEFAULT NULL,
+  `hotelId` varchar(255) DEFAULT NULL,
+  `regionId` varchar(255) DEFAULT NULL,
+  `supplierCode` varchar(3) DEFAULT NULL,
+  `supplierHotelId` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ReplacementTokens`
+--
+
+DROP TABLE IF EXISTS `ReplacementTokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ReplacementTokens` (
+  `ReplacementRule_id` bigint(20) NOT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  `pattern` varchar(255) NOT NULL,
+  PRIMARY KEY (`ReplacementRule_id`,`pattern`),
+  KEY `FK_dw2cvawgrktx58gc7wim0atxi` (`ReplacementRule_id`),
+  CONSTRAINT `FK_dw2cvawgrktx58gc7wim0atxi` FOREIGN KEY (`ReplacementRule_id`) REFERENCES `DictionaryReplacementRule` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-11-02 12:38:58
