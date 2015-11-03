01d474ee1976c7c08569df3a60d771a94adc7e05
osvaldo_lamas <osvaldolamas@gmail.com>
2015-11-03 16:46:28 -0300

    Se modifican clases despues de aplicar format.

--- destination-mapper/src/main/java/com/cocha/hotels/destinationmapper/mapping/GeoMappingService.java
+++ destination-mapper/src/main/java/com/cocha/hotels/destinationmapper/mapping/GeoMappingService.java
@@ -18,8 +18,8 @@ import com.vividsolutions.jts.geom.impl.CoordinateArraySequence;
 public class GeoMappingService {
 
     private static final double KILOMETERS_TO_MILE_FACTOR = 0.62;
-	private static final double KILOMETERS_CONVERSION = 1000.00;
-	private Logger logger = Logger.getLogger(GeoMappingService.class);
+    private static final double KILOMETERS_CONVERSION = 1000.00;
+    private Logger logger = Logger.getLogger(GeoMappingService.class);
 
     public boolean validatePointInArea(final RegionCoordinates neighborhoodArea, final Double latitude,
             final Double longitude) {
@@ -60,21 +60,21 @@ public class GeoMappingService {
         final Point point = gf.createPoint(coord);
         return point.within(polygon);
     }
-    
-	public double calculateDistance(final Double latitud1, final Double longitud1, final Double latitude,
-			final Double longitud) {
-		double distance = 0;
-		double miles = 0;
-		try {
-			GeodeticCalculator geodeticCalculator = new GeodeticCalculator();
-			geodeticCalculator.setStartingGeographicPoint(longitud1, latitud1);
-			geodeticCalculator.setDestinationGeographicPoint(longitud, latitude);
-			distance = geodeticCalculator.getOrthodromicDistance();
-			miles = (distance / KILOMETERS_CONVERSION) * KILOMETERS_TO_MILE_FACTOR;
-		} catch (Exception e) {
 
-		}
-		return miles;
-	}
+    public double calculateDistance(final Double latitud1, final Double longitud1, final Double latitude,
+            final Double longitud) {
+        double distance = 0;
+        double miles = 0;
+        try {
+            GeodeticCalculator geodeticCalculator = new GeodeticCalculator();
+            geodeticCalculator.setStartingGeographicPoint(longitud1, latitud1);
+            geodeticCalculator.setDestinationGeographicPoint(longitud, latitude);
+            distance = geodeticCalculator.getOrthodromicDistance();
+            miles = (distance / KILOMETERS_CONVERSION) * KILOMETERS_TO_MILE_FACTOR;
+        } catch (Exception e) {
+
+        }
+        return miles;
+    }
 
 }
--- destination-mapper/src/main/java/com/cocha/hotels/destinationmapper/processors/CoordinateMapperProcessorImpl.java
+++ destination-mapper/src/main/java/com/cocha/hotels/destinationmapper/processors/CoordinateMapperProcessorImpl.java
@@ -23,13 +23,13 @@ import com.cocha.hotels.model.content.mapping.RegionHotelMapping;
 public class CoordinateMapperProcessorImpl implements CoordinateMapperProcessor {
 
     private Logger logger = Logger.getLogger(CoordinateMapperProcessorImpl.class);
-    
+
     @Value("${destination.mapper.poi.distance}")
     private Double poiDistance;
-    
+
     @Autowired
     private RegionCoordinatesRepository regionCoordinatesRepository;
-    
+
     @Autowired
     private RegionRepository regionRepository;
 
@@ -42,63 +42,67 @@ public class CoordinateMapperProcessorImpl implements CoordinateMapperProcessor 
     public List<RegionHotelMapping> process(List<Hotel> hotels) {
         List<RegionHotelMapping> mapping = new ArrayList<RegionHotelMapping>();
         if (hotels != null & hotels.size() > 0) {
-            //List<RegionCoordinates> neighborhoodAreas = (List<RegionCoordinates>) regionCoordinatesRepository.findByRegionType(RegionType.NEIGHBORHOOD);
-            List<RegionCoordinates> cityAreas = (List<RegionCoordinates>) regionCoordinatesRepository.findByRegionType(RegionType.CITY);            
+            // List<RegionCoordinates> neighborhoodAreas =
+            // (List<RegionCoordinates>)
+            // regionCoordinatesRepository.findByRegionType(RegionType.NEIGHBORHOOD);
+            List<RegionCoordinates> cityAreas = (List<RegionCoordinates>) regionCoordinatesRepository
+                    .findByRegionType(RegionType.CITY);
             for (Hotel hotel : hotels) {
                 HotelMapping hotelMapping = hotelMappingContentRepository.findByhotelIdAndSupplierCode(hotel.getId(),
                         Hotel.BOOKING_SUPPLIER_CODE);
-                
+
                 String supplierHotelId = hotelMapping.getSupplierHotelId();
                 logger.info("Started mapping regions-hotels. hotel id: " + hotel.getId());
                 for (RegionCoordinates cityArea : cityAreas) {
                     if (mappingService.validatePointInArea(cityArea, hotel.getLatitude(), hotel.getLongitude())) {
-                    	//adding city
-                    	RegionHotelMapping regionHotelMapping = createRegionHotelMappingContent(supplierHotelId,
+                        // adding city
+                        RegionHotelMapping regionHotelMapping = createRegionHotelMappingContent(supplierHotelId,
                                 cityArea.getId(), hotel);
-                        mapping.add(regionHotelMapping);                    	
-                    	
-                    	//adding parent city
-                    	Region cityRegion = regionRepository.findOne(cityArea.getId());
-                    	if(cityRegion!=null){
-                    		RegionHotelMapping parentRegionHotelMapping = createRegionHotelMappingContent(supplierHotelId,
-                                    cityRegion.getIdParent(), hotel);
+                        mapping.add(regionHotelMapping);
+
+                        // adding parent city
+                        Region cityRegion = regionRepository.findOne(cityArea.getId());
+                        if (cityRegion != null) {
+                            RegionHotelMapping parentRegionHotelMapping = createRegionHotelMappingContent(
+                                    supplierHotelId, cityRegion.getIdParent(), hotel);
                             mapping.add(parentRegionHotelMapping);
-                            
-                            //adding Point of Interest
+
+                            // adding Point of Interest
                             List<Region> cityRegions = regionRepository.findByIdParent(cityRegion.getId());
                             for (Region point : cityRegions) {
-                            	
-								if (point.getRegionType().equals(RegionType.POINT_OF_INTEREST)
-										|| point.getRegionType().equals(RegionType.POINT_OF_INTEREST_SHADOW)) {
-									RegionCoordinates pointRegionCoordinates = regionCoordinatesRepository
-											.findOne(point.getId());
-									String[] pointCoordinate = pointRegionCoordinates.getCoordinates().split(";");
-									if (mappingService.calculateDistance(Double.valueOf(pointCoordinate[0]),
-											Double.valueOf(pointCoordinate[1]), hotel.getLatitude(),
-											hotel.getLongitude()) > poiDistance) {
-										RegionHotelMapping pointOfInterestHotelMapping = createRegionHotelMappingContent(
-												supplierHotelId, point.getId(), hotel);
-										mapping.add(pointOfInterestHotelMapping);
-										logger.info("Mapping regions-hotels. hotel id: " + hotel.getId()
-		                                + " was mapped succesfully into RegionCoordinate: POI " + point.getId());
-									}
-								} else {
-									if (point.getRegionType().equals(RegionType.NEIGHBORHOOD)) {
-										RegionCoordinates neighborhoodArea = regionCoordinatesRepository
-												.findOne(point.getId());
-										if (mappingService.validatePointInArea(neighborhoodArea, hotel.getLatitude(),
-												hotel.getLongitude())) {
-											RegionHotelMapping neighborhoodHotelMapping = createRegionHotelMappingContent(
-													supplierHotelId, point.getId(), hotel);
-											mapping.add(neighborhoodHotelMapping);
-											logger.info("Mapping regions-hotels. hotel id: " + hotel.getId()
-			                                + " was mapped succesfully into RegionCoordinate: NEIGHBORHOOD " + point.getId());
-										}
-									}
-								}
-							}
-                            
-                    	}                       
+
+                                if (point.getRegionType().equals(RegionType.POINT_OF_INTEREST)
+                                        || point.getRegionType().equals(RegionType.POINT_OF_INTEREST_SHADOW)) {
+                                    RegionCoordinates pointRegionCoordinates = regionCoordinatesRepository
+                                            .findOne(point.getId());
+                                    String[] pointCoordinate = pointRegionCoordinates.getCoordinates().split(";");
+                                    if (mappingService.calculateDistance(Double.valueOf(pointCoordinate[0]),
+                                            Double.valueOf(pointCoordinate[1]), hotel.getLatitude(),
+                                            hotel.getLongitude()) > poiDistance) {
+                                        RegionHotelMapping pointOfInterestHotelMapping = createRegionHotelMappingContent(
+                                                supplierHotelId, point.getId(), hotel);
+                                        mapping.add(pointOfInterestHotelMapping);
+                                        logger.info("Mapping regions-hotels. hotel id: " + hotel.getId()
+                                                + " was mapped succesfully into RegionCoordinate: POI " + point.getId());
+                                    }
+                                } else {
+                                    if (point.getRegionType().equals(RegionType.NEIGHBORHOOD)) {
+                                        RegionCoordinates neighborhoodArea = regionCoordinatesRepository.findOne(point
+                                                .getId());
+                                        if (mappingService.validatePointInArea(neighborhoodArea, hotel.getLatitude(),
+                                                hotel.getLongitude())) {
+                                            RegionHotelMapping neighborhoodHotelMapping = createRegionHotelMappingContent(
+                                                    supplierHotelId, point.getId(), hotel);
+                                            mapping.add(neighborhoodHotelMapping);
+                                            logger.info("Mapping regions-hotels. hotel id: " + hotel.getId()
+                                                    + " was mapped succesfully into RegionCoordinate: NEIGHBORHOOD "
+                                                    + point.getId());
+                                        }
+                                    }
+                                }
+                            }
+
+                        }
                         logger.info("Mapping regions-hotels. hotel id: " + hotel.getId()
                                 + " was mapped succesfully into RegionCoordinate: CITY " + cityArea.getId());
                         break;
