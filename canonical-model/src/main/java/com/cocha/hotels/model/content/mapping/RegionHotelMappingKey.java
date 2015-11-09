6b584345931ae7d55e4d092de8e81338c9bc0881
osvaldo_lamas <osvaldolamas@gmail.com>
2015-11-09 11:29:12 -0300

    Se cambia la referencia al RegionHotelMapping.Se remueve endpoint del HotelRoute.

--- canonical-model/src/main/java/com/cocha/hotels/model/content/mapping/RegionHotelMappingKey.java
+++ canonical-model/src/main/java/com/cocha/hotels/model/content/mapping/RegionHotelMappingKey.java
@@ -8,19 +8,9 @@ public class RegionHotelMappingKey implements Serializable {
 
     private static final long serialVersionUID = 1583125273900651441L;
 
-    private String id;
-
     private String regionId;
 
     private String hotelId;
-
-    public String getId() {
-        return id;
-    }
-
-    public String getRegionId() {
-        return regionId;
-    }
 
     public void setRegionId(String regionId) {
         this.regionId = regionId;
@@ -34,21 +24,16 @@ public class RegionHotelMappingKey implements Serializable {
         this.hotelId = hotelId;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     @Override
     public int hashCode() {
-        return Objects.hashCode(id, regionId, hotelId);
+        return Objects.hashCode(regionId, hotelId);
     }
 
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof RegionHotelMappingKey) {
             final RegionHotelMappingKey other = (RegionHotelMappingKey) obj;
-            return Objects.equal(id, other.id) && Objects.equal(regionId, other.regionId)
-                    && Objects.equal(hotelId, other.hotelId);
+            return Objects.equal(regionId, other.regionId) && Objects.equal(hotelId, other.hotelId);
         } else {
             return false;
         }
