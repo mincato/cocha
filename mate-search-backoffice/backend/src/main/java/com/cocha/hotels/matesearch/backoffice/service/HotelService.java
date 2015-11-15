000192065c14466196c23558302ae2dc38400c9a
germanio <german.hohmann@gmail.com>
2015-11-15 17:58:17 -0300

    Expone API Rest para hoteles del feed.

---
+++ mate-search-backoffice/backend/src/main/java/com/cocha/hotels/matesearch/backoffice/service/HotelService.java
@@ -0,0 +1,30 @@
+package com.cocha.hotels.matesearch.backoffice.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cocha.hotels.matesearch.backoffice.repository.feeds.HotelRepository;
+import com.cocha.hotels.matesearch.backoffice.service.exception.NotFoundException;
+import com.cocha.hotels.model.content.hotel.Hotel;
+
+@Service
+public class HotelService {
+
+    @Autowired
+    private HotelRepository hotelRepository;
+
+    public List<Hotel> getAll() {
+        return hotelRepository.findAll();
+    }
+
+    public Hotel find(Long id) {
+        Hotel hotel = hotelRepository.findOne(id);
+        if (hotel == null) {
+            throw new NotFoundException();
+        }
+        return hotel;
+    }
+
+}
---
+++ mate-search-backoffice/backend/src/main/java/com/cocha/hotels/matesearch/backoffice/service/rest/HotelRestService.java
@@ -0,0 +1,56 @@
+package com.cocha.hotels.matesearch.backoffice.service.rest;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cocha.hotels.matesearch.backoffice.service.HotelService;
+import com.cocha.hotels.matesearch.backoffice.util.RestResponseHandler;
+import com.cocha.hotels.model.content.hotel.Hotel;
+
+@Service
+@Path("/hoteles")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class HotelRestService {
+
+	@Autowired
+	private HotelService hotelService;
+	
+    @Autowired
+    private RestResponseHandler responseHandler;
+	
+    @GET
+    @Path("{id}")
+    public Response getHotel(@Context HttpServletRequest request, @PathParam("id") Long id) {
+        try {
+            Hotel hotel = hotelService.find(id);
+            return responseHandler.buildSuccessResponse(hotel, Status.OK);
+        } catch (Exception e) {
+            return responseHandler.buildErrorResponse(e);
+        }    	
+    }
+	
+	@GET
+	public Response getAll(@Context HttpServletRequest request) {
+        try {
+        	List<Hotel> hoteles = hotelService.getAll();
+            return responseHandler.buildSuccessResponse(hoteles, Status.OK);
+        } catch (Exception e) {
+            return responseHandler.buildErrorResponse(e);
+        }
+	}
+	
+}
